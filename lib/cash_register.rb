503463ff5ce382611148503b7addab59079ab29f
Annie Phan <anniephan.atp@gmail.com>
2019-03-25 05:09:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,4 +2,7 @@ class CashRegister
   attr_accessor :total, :discount, :last_transaction_amt, :items
 
   def initialize(discount = 0)
+    @total = 0 
+    @discount = discount
+    @items = []
 end
